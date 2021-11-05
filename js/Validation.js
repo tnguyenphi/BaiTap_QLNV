991908338da99fa157d3863e44c3fd742d193198
NguyenPhi <nguyenphidhbk@gmail.com>
2021-11-05 11:39:50 +0700

    Update ngay 5/11/2021

---
+++ js/Validation.js
@@ -0,0 +1,105 @@
+function Validation() {
+    //Kiểm tra rỗng
+
+    this.checkEmpty = function (value, message, spanID) {
+        if (value.trim() != "") {
+            //hợp lệ
+            document.getElementById(spanID).innerHTML = "";
+            document.getElementById(spanID).style.display = "none";
+            return true;
+        }
+        //không hợp lệ
+        document.getElementById(spanID).innerHTML = message;
+        document.getElementById(spanID).style.display = "block";
+        return false;
+    }
+
+    //kiểm tra tên tk không được trùng
+
+    this.checkID = function (value, message, spanID, mangNV) {
+        // giã sử  tên tk chưa tồn tại trongmangr
+        var isExist = false;
+
+        isExist = mangNV.some(function (nv) {
+            return value == nv.taiKhoanNV
+        });
+        //có tồn tại mã trùng
+        if (isExist) {
+            //không hợp lệ
+            document.getElementById(spanID).innerHTML = message;
+            document.getElementById(spanID).style.display = "block";
+            return false
+        } else {
+            document.getElementById(spanID).innerHTML = "";
+            document.getElementById(spanID).style.display = "none";
+            return true;
+        }
+    }
+
+
+    // Kiểm tra tên NV
+
+    this.checkName = function (value, message, spanID) {
+        //kiểu string
+        var pattern = "^[a-zA-Z_ÀÁÂÃÈÉÊẾÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶ" + "ẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợ" + "ụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹý\\s]+$";
+
+        //chuyển kiểu từ string sang RegExp
+        var reg = new RegExp(pattern);
+
+        if (reg.test(value)) {
+            //hợp lệ
+            document.getElementById(spanID).innerHTML = "";
+            document.getElementById(spanID).style.display = "none";
+            return true;
+        }
+        //không hợp lệ
+        document.getElementById(spanID).innerHTML = message;
+        document.getElementById(spanID).style.display = "block";
+        return false;
+    }
+
+    //kiểm định dạng email
+
+    this.checkEmail = function (value, message, spanID) {
+        // Chuỗi Regexp
+        var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (value.match(pattern)) {
+            //hợp lệ
+            document.getElementById(spanID).innerHTML = "";
+            document.getElementById(spanID).style.display = "none";
+            return true;
+        }
+        // Không hợp lệ 
+        document.getElementById(spanID).innerHTML = message;
+        document.getElementById(spanID).style.display = "block";
+        return false;
+    }
+
+    //check pass
+    this.checkPass = function(value, message, spanID){
+        var pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,8}$/;
+        if (value.match(pattern)){
+            document.getElementById(spanID).innerHTML = "";
+            document.getElementById(spanID).style.display = "none";
+            return true;
+        }
+        document.getElementById(spanID).innerHTML = message;
+        document.getElementById(spanID).style.display = "block";
+        return false;
+    }
+
+    //Check Select
+    this.checkSelect = function (selectID, message, spanID){
+        if (document.getElementById(selectID).selectedIndex != 0 ){
+            //hợp lệ
+            document.getElementById(spanID).innerHTML = "";
+            document.getElementById(spanID).style.display = "none";
+            return true;   
+        }
+        // không hợp lệ
+        document.getElementById(spanID).innerHTML = message;
+        document.getElementById(spanID).style.display = "block";
+        return false;
+    }
+
+}
--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 
 
 var dsnv = new DanhSachNhanVien();
+var validation = new Validation();
 
-
-function getELE(id){
+function getELE(id) {
     return document.getElementById(id);
 }
 
@@ -21,34 +21,55 @@ function getLocalStorage(mangNV) {
 }
 getLocalStorage();
 
-function layThongTinNV (){
-    var taiKhoanNV = getELE("tknv").value ;
-    var tenNV = getELE("name").value ;
-    var email = getELE("email").value ;
-    var matKhau = getELE("password").value ;
-    var date = getELE("datepicker").value ;
-    var salary = getELE("luongCB").value ;
-    var position = getELE("chucvu").value ;
-    var hours = getELE("gioLam").value ;
+function layThongTinNV() {
+    var taiKhoanNV = getELE("tknv").value;
+    var tenNV = getELE("name").value;
+    var email = getELE("email").value;
+    var matKhau = getELE("password").value;
+    var date = getELE("datepicker").value;
+    var salary = getELE("luongCB").value;
+    var position = getELE("chucvu").value;
+    var hours = getELE("gioLam").value;
+
+    var isValid = true;
 
 
-    var nv = new NhanVien(taiKhoanNV,tenNV,email,matKhau,date,Number(salary),position, Number(hours));
-    nv.luong = nv.tongLuong();
-    nv.xepLoai = nv.xepLoai();
-    nv.viTri = nv.showPosition()
+    // Kiểm tra tài khoảng NV không được trùng và không được để trống
 
-    dsnv.themNV(nv);
-    console.log(dsnv.mangNV);
-    hienThiTable(dsnv.mangNV);
-    setLocalStorage(dsnv.mangNV);
+    isValid &= validation.checkEmpty(taiKhoanNV,"Tài khoản NV không được để trống","tbTKNV") && validation.checkID(taiKhoanNV,"Tài khoản NV không được trùng","tbTKNV",dsnv.mangNV);
+
+    // Kiểm tra tên NV: Kiểm tra rỗng, kiểm tra ký tự chữ
+    isValid &= validation.checkEmpty(tenNV,"Tên NV không được để trống","tbTen") && validation.checkName(tenNV,"Tên NV Phải là kiểu chữ","tbTen");
+
+    // Kiểm tra email: Email phải đúng định dạng và không để trống
+    isValid &= validation.checkEmpty(email,"Email không được để trống","tbEmail") && validation.checkEmail(email,"Email phải đúng định dạng","tbEmail");
+    
+    //kiểm tra pass: không được để trống và phải đúng địng dạng pass
+    isValid &= validation.checkEmpty(matKhau,"Password không được để trống","tbMatKhau") && validation.checkPass(matKhau,"Password phải đúng định dạng","tbMatKhau");
+    //Kiểm tra vị trí phải chọn vị trí
+    isValid &= validation.checkSelect("chucvu","Chọn chức vụ","tbChucVu") ;
+
+
+    if (isValid) {
+        var nv = new NhanVien(taiKhoanNV, tenNV, email, matKhau, date, Number(salary), position, Number(hours));
+        nv.luong = nv.tongLuong();
+        nv.xepLoai = nv.xepLoai();
+        nv.viTri = nv.showPosition()
+
+        dsnv.themNV(nv);
+        console.log(dsnv.mangNV);
+        hienThiTable(dsnv.mangNV);
+        setLocalStorage(dsnv.mangNV);
+    }
+
 }
 getELE("btnThemNV").onclick = layThongTinNV;
 
 //hiển thị thông tin lên table
 
-function hienThiTable(mangNV){
+function hienThiTable(mangNV) {
     var content = "";
-    for (var i = 0; i < mangNV.length; i++){
+    for (var i = 0; i < mangNV.length; i++) {
         var trNV = `<tr>
         <td>${mangNV[i].taiKhoanNV}</td>
         <td>${mangNV[i].tenNV}</td> 
@@ -64,28 +85,28 @@ function hienThiTable(mangNV){
             <button onclick="xemChiTiet('${mangNV[i].taiKhoanNV}')" data-target="#myModal" data-toggle="modal" class ="btn btn-info">Info</button>
         </td>    
     </tr>`
-    content += trNV;
+        content += trNV;
     }
     console.log(content);
     getELE("tableDanhSach").innerHTML = content;
 }
 
-function xoaNhanVien(tk){
+function xoaNhanVien(tk) {
     dsnv.xoaNV(tk);
     setLocalStorage(dsnv.mangNV);
     hienThiTable(dsnv.mangNV);
 }
 //reset form mỗi khi nhấn vào button thêm sinh viên
-function resetForm(){
+function resetForm() {
     getELE("resetForm").reset();
     getELE("tknv").disabled = false;
 }
 
 //Lấy thông tin chi tiết nv đẩy lên form
-function xemChiTiet(tk){
+function xemChiTiet(tk) {
     var nvTimDuoc = dsnv.layChiTiet(tk);
     console.log("Nhân viên tìm được")
-    if(nvTimDuoc != undefined){
+    if (nvTimDuoc != undefined) {
         //tìm được nhân viên
         getELE("tknv").disabled = true;
         getELE("tknv").value = nvTimDuoc.taiKhoanNV;
@@ -96,29 +117,29 @@ function xemChiTiet(tk){
         getELE("luongCB").value = nvTimDuoc.salary;
         getELE("chucvu").value = nvTimDuoc.position;
         getELE("gioLam").value = nvTimDuoc.hours;
-    }else{
+    } else {
         console.log("Không tìm được")
     }
 }
 
 
 
-function capNhat(){
-    var taiKhoanNV = getELE("tknv").value ;
-    var tenNV = getELE("name").value ;
-    var email = getELE("email").value ;
-    var matKhau = getELE("password").value ;
-    var date = getELE("datepicker").value ;
-    var salary = Number(getELE("luongCB").value) ;
-    var position = getELE("chucvu").value ;
-    var hours = Number(getELE("gioLam").value) ;
+function capNhat() {
+    var taiKhoanNV = getELE("tknv").value;
+    var tenNV = getELE("name").value;
+    var email = getELE("email").value;
+    var matKhau = getELE("password").value;
+    var date = getELE("datepicker").value;
+    var salary = Number(getELE("luongCB").value);
+    var position = getELE("chucvu").value;
+    var hours = Number(getELE("gioLam").value);
 
 
-    var nv = new NhanVien(taiKhoanNV,tenNV,email,matKhau,date,Number(salary),position, Number(hours));
+    var nv = new NhanVien(taiKhoanNV, tenNV, email, matKhau, date, Number(salary), position, Number(hours));
     nv.luong = nv.tongLuong();
     nv.xepLoai = nv.xepLoai();
     nv.viTri = nv.showPosition();
-    
+
     dsnv.updateNV(nv);
     hienThiTable(dsnv.mangNV);
     setLocalStorage(dsnv.mangNV);
